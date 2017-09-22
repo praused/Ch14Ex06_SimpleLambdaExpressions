55299615eef82249b301ca7d6582907db7316ee6
David Prause <praused@hotmail.com>
2017-09-22 11:11:23 -0400

    Add project files.

---
+++ Ch14Ex06_SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch14Ex06_SimpleLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
